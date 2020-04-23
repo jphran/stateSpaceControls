a343c2aa29661e08c13f3104d16a3a0dbc9ca0ac
JUSTIN FRANCIS <u1063296@umail.utah.edu>
2020-04-23 16:22:08 -0600

    check pt

--- DC_Motor/DC_Motor_Analysis.m
+++ DC_Motor/DC_Motor_Analysis.m
@@ -16,9 +16,11 @@ tach = medfilt1(tach,20);
 
 p = polyfit(t(3500:end), enc(3500:end), 2);
 
+dcGainByInspection = 45;
+
 %% part 2
 A = [-2.6788 0;1 0];
-B = [12.05465;0];
+B = [120.5465;0];
 C = [0 1];
 D = [0];
 
@@ -35,31 +37,33 @@ K = place(A, B, [p, (p+.00001)]);
 
 dcGain = C * inv(-(A-B*K))* B + D;
 feedForwardGain = 1/dcGain;
-
+encGain = (2*pi)/4096; %[rad/counts]
 %simulink feedback analysis
-% sim('Sim2.slx');
-% vel = out.vel;
+% sim('fullStateFeedback.slx');
+pos = out.pos;
 % t = out.tout;
 % 
 % figure();
 % plot(t,vel);
-
-%% settling time
-Ts_check = vel > 0.96*vel(end);
+% 
+% settling time
+Ts_check = pos > 0.96*pos(end);
 t_check = t(Ts_check);
 
 Ts_sim = t_check(1);
 
-%% rmse steady state
-velDes = 1;
-vel_check = vel(Ts_check);
+% rmse steady state
+posDes = 1;
+pos_check = pos(Ts_check);
 
-RSME = sqrt(mean((vel_check - velDes).^2));
+RSME = sqrt(mean((pos_check - posDes).^2));
 
 %% robust tracking and distrubance rejection
 [num, den] = ss2tf(A,B,C,D);
 zeros = roots(num);
 % its controllable because no zeros at s = 0
+
+initCond = 0;
 
 syms ka k1 k2
 %char eqn desired: s^3+12s^2+48s+64
@@ -82,6 +86,54 @@ K = double(linsolve(meatMat, potatoesMat));
 K1 = K(1);
 K2 = K(2);
 Ka = K(3);
+K = [K1 K2];
 Acheck = [[A+B*[K1 K2] B*Ka];[-C 0]];
-Bcheck = [0 0 1];
-Kcheck = place(Acheck,B, [p+.0001 p-.0001 p]); 
+Bcheck = [0 0 1].';
+Kcheck = place(Acheck,Bcheck, [p+.0001 p-.0001 p]); 
+
+pos = out.pos;
+
+% settling time
+Ts_check = pos > 0.96*pos(end);
+t_check = t(Ts_check);
+
+Ts_sim = t_check(1);
+
+% rmse steady state
+posDes = 1;
+pos_check = pos(Ts_check);
+
+RSME = sqrt(mean((pos_check - posDes).^2));
+
+%% part 4, LQR
+initCond = -1;
+stepInput = 0;
+
+%finding A_tilde 
+syms k1 k2 ka
+K_sym = [k1 k2 ka];
+B_tilde = [-120.5465 0 0].';
+A_tilde = double(An + B*K_sym);
+
+C_tilde = [0 0 1];
+D_tilde = [0];
+sys = ss(A_tilde, B_tilde, C_tilde, D_tilde);
+
+%diag
+q11 = 1;
+q22 = 1;
+q33 = 1;
+%off diag
+q12 = 0;
+q13 = 0;
+q23 = 0;
+
+Q = [[q11 q12 q13];[q12 q22 q23];[q13 q23 q33]]; %cost applied to states
+
+
+R = 1; %cost applied to inputs
+
+[K_tilde, S, CLP] = lqr(sys,Q,R);
+
+K = K_tilde(1:2);
+Ka = K_tilde(end);
